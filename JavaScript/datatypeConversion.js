7485d3701d9972d63cb774d20bceb7802a904b3c
Bhuwan Pandit <panditbhuwan2@gmail.com>
2023-09-06 17:01:48 +0545

    Data type  conversion

---
+++ JavaScript/datatypeConversion.js
@@ -0,0 +1,39 @@
+let n = 1;
+
+console.log(typeof(n)); // number
+
+n ="hello"; // dyanmic type
+console.log(typeof(n));
+
+let num1 = "3";
+let num2 = 4;
+
+console.log(num1+num2);
+
+
+let num3 = "3";
+let num4 = 4;
+console.log(parseInt(num3)+num4);// converison from string to number
+
+let num5 = "3.5";
+let num6 = 4;
+console.log(parseFloat(num5)+num6);// conversion string to float
+
+
+let num7 = "3.5";
+let num8 = 9;
+console.log(+num7+num8);//addition
+
+let num9 = "3.5";
+let num10 = 9;
+console.log(num9-num10);// subtraction
+
+
+console.log(num9*num10);// multiplication
+
+console.log(num9/num10);// division
+
+
+n9 ="101";
+
+console.log(parseInt(n9,16));// converison from binary to hexadecimal
