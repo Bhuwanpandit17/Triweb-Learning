233d3f0cbf3d956cec91bad1dd95f9760f813c33
Bhuwan Pandit <panditbhuwan2@gmail.com>
2023-09-06 17:14:39 +0545

    literal pushed

---
+++ JavaScript/literal.js
@@ -0,0 +1,20 @@
+
+console.log("literal");
+console.log("decimal", 117);
+console.log("octal", 0o77);
+console.log("hexa decimal", 0x00111);
+console.log("binary", 0b00011);
+console.log("Binary", 0o00011);
+
+console.log("object literal");
+
+let obj={name:"bhuwan", mobile:"88975"};
+console.log(obj);
+console.log(obj.name);
+console.log(obj.mobile);
+let obj1={" ":"bhuwan", "!":"88975"};
+console.log(obj1);
+console.log(obj1[" "]);
+console.log(obj1["!"]);
+
+
