f99f7a257210301a7e2a427a14b6cb9fe2e5d47b
Bhuwan Pandit <panditbhuwan2@gmail.com>
2023-09-06 19:00:03 +0545

    Finally handling

---
+++ JavaScript/expectionFinally.js
@@ -0,0 +1,38 @@
+
+
+function validateUser(name, password){
+
+
+try{
+
+    if (name.length<5){
+        throw new Error("User name is too short");
+    }
+
+    if (password.length<6){
+        throw new Error("invalid password");
+    }
+    console.log("working");
+
+
+
+    return "in try"
+}
+catch(error){
+
+    console.log(error);
+    return"Fail in catch";
+
+}
+
+finally{
+    console.log("Inside finally block");
+    return "from finally";
+}
+
+}
+
+let name = "Bhu";
+let password = "12345677";
+let mgs = validateUser(name, password);
+console.log("user", mgs);
