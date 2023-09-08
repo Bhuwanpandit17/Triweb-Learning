3a7f9a53ee97a5ba50a89eb8757c6b54384c4c42
Bhuwan Pandit <panditbhuwan2@gmail.com>
2023-09-08 15:52:56 +0545

    Condition based function

---
+++ JavaScript/conditionBasedFunction.js
@@ -0,0 +1,22 @@
+  
+//let decideFun ="add";
+ let decideFun = "subtract";
+
+let getResult;
+
+if (decideFun=="add"){
+    getResult=function(num1,num2){
+        console.log(num1+num2);
+
+    }
+
+}else if(decideFun=="subtract")
+{
+    getResult=function(num1, num2){
+    console.log(num1-num2);
+    }
+    
+}
+
+getResult(5,4);
+
