9408dd00b7cf2d4a63eaf0e0b2b27b762d066ee9
Bhuwan Pandit <panditbhuwan2@gmail.com>
2023-09-08 22:27:40 +0545

    creating array

---
+++ JavaScript/creatingArray.js
@@ -0,0 +1,25 @@
+ 
+ let arr = new Array(23,23,45,67,4,32,45);
+
+ let arr1 = Array(20,23,24,25,25,2,6);
+
+ let arr2 = [3,2,5,3,7,4,8];
+
+ // populate an array 
+
+ let arr = [];
+
+ arr[0]="India";
+ arr[1]="Nepal";
+ arr[2]="US";
+
+ console.log(arr);
+
+ //length of array
+
+ let arr =[1,2,3,4,5,6,7,8,9];
+
+ console.log("the length of array", arr.length);
+
+
+ 
