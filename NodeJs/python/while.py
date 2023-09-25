6237f7baf347d8d6cc81cfe9cb31ba892b2f6fc8
Bhuwan Pandit <panditbhuwan2@gmail.com>
2023-09-25 20:05:21 +0545

    It contain the basic of programming concept in python language

---
+++ NodeJs/python/while.py
@@ -0,0 +1,7 @@
+tracker = 1                    # Initialize the variable 'tracker' with the value 1
+while tracker < 10:            # Enter the loop as long as 'tracker' is less than 10
+    print(tracker)             # Print the current value of 'tracker'
+    tracker = tracker + 1      # Increment the value of 'tracker' by 1 in each iteration
+
+# Print "Finish loop" after the loop is done executing
+print("Finish loop")
